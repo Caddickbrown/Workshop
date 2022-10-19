3e35b1c6d122c6ba47909dd71f80d4b0b57c5eac
Caddickbrown <danielcaddickbrown@gmail.com>
2022-10-19 10:35:30 +0100

    SQL Documentation

--- BVI/BacklogPower/Requisitions.sql
+++ BVI/BacklogPower/Requisitions.sql
@@ -1,12 +1,13 @@
-SELECT req.PROPOSAL_NO AS Proposal,
-req.PART_NO AS PartNo,
-req.PROP_START_DATE AS ProposedStartDate,
-req.REVISED_DUE_DATE AS RevisedDueDate,
-req.PLAN_ORDER_REC AS Qty
-FROM IFS.SHOP_ORDER_PROP_TAB AS req
-INNER JOIN IFS.INVENTORY_PART_TAB AS ipt
-	ON req.PART_NO = ipt.PART_NO AND req.CONTRACT = ipt.CONTRACT
-WHERE req.CONTRACT = '2051'
-AND req.ROWSTATE = 'ProposalCreated'
-AND req.PROP_START_DATE < (GETDATE ()+63)
-AND ipt.PLANNER_BUYER IN ('3001','3801','5001');
+SELECT
+req.PROPOSAL_NO AS Proposal, -- Proposal Number
+req.PART_NO AS PartNo, -- Part Number
+req.PROP_START_DATE AS ProposedStartDate, -- Proposed Start Date
+req.REVISED_DUE_DATE AS RevisedDueDate, -- Expected Due Date
+req.PLAN_ORDER_REC AS Qty -- Lot Size (Quantity)
+FROM IFS.SHOP_ORDER_PROP_TAB AS req -- From Shop Order Requisitions Info
+INNER JOIN IFS.INVENTORY_PART_TAB AS ipt -- Connect to Inventory Part Info
+	ON req.PART_NO = ipt.PART_NO AND req.CONTRACT = ipt.CONTRACT -- Join on Part Number and Site
+WHERE req.CONTRACT = '2051' -- Only on Site 2051 (Bidford)
+AND req.ROWSTATE = 'ProposalCreated' -- Only Proposals
+AND req.PROP_START_DATE < (GETDATE ()+63) -- Only the data for the next 9 weeks
+AND ipt.PLANNER_BUYER IN ('3001','3801','5001'); -- Only Kit Data
--- BVI/BacklogPower/Shop Orders.sql
+++ BVI/BacklogPower/Shop Orders.sql
@@ -1,12 +1,12 @@
 SELECT
-so.ORDER_NO,
-so.PART_NO,
-so.REVISED_START_DATE,
-so.REVISED_QTY_DUE
-FROM IFS.SHOP_ORD_TAB AS so
-INNER JOIN IFS.INVENTORY_PART_TAB AS ipt
-	ON so.PART_NO = ipt.PART_NO AND so.CONTRACT = ipt.CONTRACT
-WHERE so.CONTRACT = '2051'
-AND so.ROWSTATE = 'Released'
-AND so.REVISED_START_DATE < (GETDATE ()+63)
-AND ipt.PLANNER_BUYER IN ('3001','3801','5001');
+so.ORDER_NO, -- Shop Order Number
+so.PART_NO, -- Part Number
+so.REVISED_START_DATE, -- Current Start Date
+so.REVISED_QTY_DUE -- Currrent Qty
+FROM IFS.SHOP_ORD_TAB AS so -- From the Shop Order Information
+INNER JOIN IFS.INVENTORY_PART_TAB AS ipt -- Connect to Inventory Part Info
+	ON so.PART_NO = ipt.PART_NO AND so.CONTRACT = ipt.CONTRACT -- Join on Part Number and Site
+WHERE so.CONTRACT = '2051' -- Only on Site 2051 (Bidford)
+AND so.ROWSTATE = 'Released' -- Only Released Orders
+AND so.REVISED_START_DATE < (GETDATE ()+63) -- Only the data for the next 9 weeks
+AND ipt.PLANNER_BUYER IN ('3001','3801','5001'); -- Only Kit Data
